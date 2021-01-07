e59a156491c2dff34f720590d8e1c1682ddb27b1
Christopher Man <christophergayiuman@gmail.com>
2021-01-07 17:03:57 +0800

    Update

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# Establishing the driver
+PATH = 'C:\webdrivers\chromedriver.exe'
+driver = webdriver.Chrome(PATH)
 
-#Establishing the driver
-PATH = ''
+
+def get_website_url(userAnswer):
+    if userAnswer == 1:
+        requested_url = "https://www.facebook.com/login/"
+    if userAnswer == 2:
+        requested_url = "https://twitter.com/login"
+    if userAnswer == 3:
+        requested_url = "https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F"
+    return requested_url
+
+
+def facebook_login(requested_url):
+    driver.get(requested_url)
+
+
+answer = get_website_url(1)
+facebook_login(answer)
